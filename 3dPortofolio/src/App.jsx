221fef205776b527de6a7dd56d5ac7e010ef1d2f
Victor Mamaliga <m.viktor24.vm@gmail.com>
2023-07-10 18:13:29 +0100

    first page done

--- 3dPortofolio/src/App.jsx
+++ 3dPortofolio/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components";
+import { Contact } from "./components/Contact";
+import {Hero} from "./components/Hero";
+import {Who} from "./components/Who";
+import {Works} from "./components/Works";
+
+
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url("./img/bg.jpeg");
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container >
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </Container>
+  );
 }
 
-export default App
+export default App;
