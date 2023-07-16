16f9868365fde4c0123b62a98842d6eb8152987e
Victor Mamaliga <m.viktor24.vm@gmail.com>
2023-07-16 19:18:58 +0100

    cube-basic

--- 3dPortofolio/src/components/Cube.jsx
+++ 3dPortofolio/src/components/Cube.jsx
@@ -1,33 +1,27 @@
-import React, { useMemo } from 'react'
-import { useLoader } from '@react-three/fiber'
-import { TextureLoader } from 'three'
+import React, { useRef } from "react";
+import { PerspectiveCamera, RenderTexture, Text } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 const Cube = () => {
-  const [texture1, texture2, texture3, texture4, texture5, texture6] = useMemo(() => {
-    const loader = new TextureLoader()
-    return [
-      loader.load('../../public/img/gtaImage1.jpg'),
-      loader.load('../../public/img/gtaImage5.png'),
-      loader.load('../../public/img/gta1.jpg'),
-      loader.load('../../public/img/gtaImage2.jpg'),
-      loader.load('../../public/img/gtaImage4.jpg'),
-      loader.load('../../public/img/gta1.jpg')
-    ]
-  }, [])
-
-  const materials = useMemo(
-    () => [texture1, texture2, texture3, texture4, texture5, texture6].map(map => ({ map })),
-    [texture1, texture2, texture3, texture4, texture5, texture6]
-  )
-
+  const textRef = useRef();
+  useFrame(
+    (state) =>
+      (textRef.current.position.x = Math.sin(state.clock.elapsedTime) * 2)
+  );
   return (
     <mesh>
-      <boxGeometry args={[1, 1, 1]} />
-      {materials.map((material, index) => (
-        <meshBasicMaterial attachArray="material" key={index} {...material} />
-      ))}
+      <boxGeometry />
+      <meshStandardMaterial>
+        <RenderTexture attach="map">
+          <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+          <color attach="background" args={["#dc9dcd"]} />
+          <Text ref={textRef} fontSize={3} color="#555">
+            hello
+          </Text>
+        </RenderTexture>
+      </meshStandardMaterial>
     </mesh>
-  )
-}
+  );
+};
 
-export default Cube
+export default Cube;
